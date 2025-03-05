cfc308df36f7c39cc48f213c9592c34540a453f3
ajaikumarvs <ajaikumarvs@gmail.com>
2025-03-05 17:12:55 +0530

    added Sections

---
+++ src/sections/Header.tsx
@@ -0,0 +1,17 @@
+export const Header = () => {
+    return (
+    <header>
+        <div className="container">
+            <div>
+                <div>
+                    <span>logo</span>
+                    <div>
+                        <button>Join waitlist</button>
+                        <span>menu</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    );
+};
